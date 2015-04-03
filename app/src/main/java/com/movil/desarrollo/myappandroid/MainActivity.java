9775d9369baceb78a503ab07d1444c7dc122d848
Jorge Atempa <atempaitm@gmail.com>
2015-04-02 18:49:46 -0700

    Se creo funcionalidad para el boton

--- app/src/main/java/com/movil/desarrollo/myappandroid/MainActivity.java
+++ app/src/main/java/com/movil/desarrollo/myappandroid/MainActivity.java
@@ -2,14 +2,34 @@ package com.movil.desarrollo.myappandroid;
 
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com.mx"));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
