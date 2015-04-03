57892796d7325c6eaa1932f810f62baffb3147e1
Jorge Atempa <atempaitm@gmail.com>
2015-04-02 20:12:47 -0700

    Ejemplo del uso de RatingBar

--- app/src/main/java/com/movil/desarrollo/myappandroid/MainActivity.java
+++ app/src/main/java/com/movil/desarrollo/myappandroid/MainActivity.java
@@ -5,39 +5,55 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.RatingBar.OnRatingBarChangeListener;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 
-    private ToggleButton toggleButton1, toggleButton2;
-    private Button btnDisplay;
+    private RatingBar ratingBar;
+    private TextView txtRatingValue;
+    private Button btnSubmit;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        addListenerOnRatingBar();
         addListenerOnButton();
     }
 
-    public void addListenerOnButton() {
-        toggleButton1 = (ToggleButton) findViewById(R.id.toggleButton1);
-        toggleButton2 = (ToggleButton) findViewById(R.id.toggleButton2);
-        btnDisplay = (Button) findViewById(R.id.btnDisplay);
+    public void addListenerOnRatingBar() {
 
-        btnDisplay.setOnClickListener(new OnClickListener() {
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        txtRatingValue = (TextView) findViewById(R.id.txtRatingValue);
+
+        //if rating value is changed,
+        //display the current rating value in the result (textview) automatically
+        ratingBar.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
+            public void onRatingChanged(RatingBar ratingBar, float rating,
+                                        boolean fromUser) {
+
+                txtRatingValue.setText(String.valueOf(rating));
+
+            }
+        });
+    }
+
+    public void addListenerOnButton() {
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        //if click on me, then display the current rating value.
+        btnSubmit.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                StringBuffer result = new StringBuffer();
-                result.append("toggleButton1 : ").append(toggleButton1.getText());
-                result.append("\ntoggleButton2 : ").append(toggleButton2.getText());
-
-                Toast.makeText(MainActivity.this, result.toString(),
+                Toast.makeText(MainActivity.this,
+                        String.valueOf(ratingBar.getRating()),
                         Toast.LENGTH_SHORT).show();
 
             }
