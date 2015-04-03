d4a69716258e334f399237d297b0bff3f2669448
Jorge Atempa <atempaitm@gmail.com>
2015-04-02 20:51:12 -0700

    Ejemplo del uso de TimePicker

--- app/src/main/java/com/movil/desarrollo/myappandroid/MainActivity.java
+++ app/src/main/java/com/movil/desarrollo/myappandroid/MainActivity.java
@@ -2,68 +2,66 @@ package com.movil.desarrollo.myappandroid;
 
 import java.util.Calendar;
 import android.app.Activity;
-import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends Activity {
 
-    private TextView tvDisplayDate;
-    private DatePicker dpResult;
-    private Button btnChangeDate;
+    private TextView tvDisplayTime;
+    private TimePicker timePicker1;
+    private Button btnChangeTime;
 
-    private int year;
-    private int month;
-    private int day;
+    private int hour;
+    private int minute;
 
-    static final int DATE_DIALOG_ID = 999;
+    static final int TIME_DIALOG_ID = 999;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setCurrentDateOnView();
+        setCurrentTimeOnView();
         addListenerOnButton();
     }
 
-    // display current date
-    public void setCurrentDateOnView() {
+    // display current time
+    public void setCurrentTimeOnView() {
 
-        tvDisplayDate = (TextView) findViewById(R.id.tvDate);
-        dpResult = (DatePicker) findViewById(R.id.dpResult);
+        tvDisplayTime = (TextView) findViewById(R.id.tvTime);
+        timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
 
         final Calendar c = Calendar.getInstance();
-        year = c.get(Calendar.YEAR);
-        month = c.get(Calendar.MONTH);
-        day = c.get(Calendar.DAY_OF_MONTH);
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
 
-        // set current date into textview
-        tvDisplayDate.setText(new StringBuilder()
-                // Month is 0 based, just add 1
-                .append(month + 1).append("-").append(day).append("-")
-                .append(year).append(" "));
+        // set current time into textview
+        tvDisplayTime.setText(
+                new StringBuilder().append(pad(hour))
+                        .append(":").append(pad(minute)));
 
-        // set current date into datepicker
-        dpResult.init(year, month, day, null);
+        // set current time into timepicker
+        timePicker1.setCurrentHour(hour);
+        timePicker1.setCurrentMinute(minute);
 
     }
 
     public void addListenerOnButton() {
 
-        btnChangeDate = (Button) findViewById(R.id.btnChangeDate);
+        btnChangeTime = (Button) findViewById(R.id.btnChangeTime);
 
-        btnChangeDate.setOnClickListener(new OnClickListener() {
+        btnChangeTime.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                showDialog(DATE_DIALOG_ID);
+                showDialog(TIME_DIALOG_ID);
 
             }
 
@@ -74,32 +72,37 @@ public class MainActivity extends Activity {
     @Override
     protected Dialog onCreateDialog(int id) {
         switch (id) {
-            case DATE_DIALOG_ID:
-                // set date picker as current date
-                return new DatePickerDialog(this, datePickerListener,
-                        year, month,day);
+            case TIME_DIALOG_ID:
+                // set time picker as current time
+                return new TimePickerDialog(this,
+                        timePickerListener, hour, minute,false);
+
         }
         return null;
     }
 
-    private DatePickerDialog.OnDateSetListener datePickerListener
-            = new DatePickerDialog.OnDateSetListener() {
+    private TimePickerDialog.OnTimeSetListener timePickerListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                public void onTimeSet(TimePicker view, int selectedHour,
+                                      int selectedMinute) {
+                    hour = selectedHour;
+                    minute = selectedMinute;
 
-        // when dialog box is closed, below method will be called.
-        public void onDateSet(DatePicker view, int selectedYear,
-                              int selectedMonth, int selectedDay) {
-            year = selectedYear;
-            month = selectedMonth;
-            day = selectedDay;
+                    // set current time into textview
+                    tvDisplayTime.setText(new StringBuilder().append(pad(hour))
+                            .append(":").append(pad(minute)));
 
-            // set selected date into textview
-            tvDisplayDate.setText(new StringBuilder().append(month + 1)
-                    .append("-").append(day).append("-").append(year)
-                    .append(" "));
+                    // set current time into timepicker
+                    timePicker1.setCurrentHour(hour);
+                    timePicker1.setCurrentMinute(minute);
 
-            // set selected date into datepicker also
-            dpResult.init(year, month, day, null);
+                }
+            };
 
-        }
-    };
+    private static String pad(int c) {
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
+    }
 }
